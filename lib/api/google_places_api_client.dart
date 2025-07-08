38b990a00972e8ffa912eba3f046080f98a536e5
YukiNaruse <nsingularityy@gmail.com>
2025-07-08 09:03:25 +0900

    コミット漏れ

--- lib/api/google_places_api_client.dart
+++ lib/api/google_places_api_client.dart
@@ -1,75 +1,25 @@
-import 'dart:convert';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+import 'package:ramen_recommendation/api/responses/get_place_details_response.dart';
+import 'package:ramen_recommendation/api/responses/search_ramen_places_response.dart';
 
-class GooglePlacesApiClient {
-  static final GooglePlacesApiClient _instance =
-      GooglePlacesApiClient._internal();
-  late final String _apiKey;
+part 'google_places_api_client.g.dart';
 
-  factory GooglePlacesApiClient() {
-    return _instance;
-  }
+@RestApi(baseUrl: "https://places.googleapis.com/v1/")
+abstract class GooglePlacesApiClient {
+  factory GooglePlacesApiClient(Dio dio, {String baseUrl}) = _GooglePlacesApiClient;
 
-  GooglePlacesApiClient._internal();
+  @POST("places:searchText")
+  Future<SearchRamenPlacesResponse> searchRamenPlaces(
+    @Body() Map<String, dynamic> requestBody,
+    @Header("X-Goog-Api-Key") String apiKey,
+    @Header("X-Goog-FieldMask") String fieldMask,
+    @Header("Content-Type") String contentType);
 
-  /// 環境変数から API キーを読み込む
-  Future<void> initialize() async {
-    const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
-    final envFileName = '.env.$flavor';
-
-    final envData = await rootBundle.loadString(envFileName);
-    final label = envData.split('\n');
-    _apiKey = label
-        .firstWhere((line) => line.startsWith('GOOGLE_MAPS_API_KEY'))
-        .split(' ')[1]
-        .trim();
-  }
-
-  /// Google Places API に POST リクエストを送信
-  Future<Map<String, dynamic>> post(
-      String endpoint, Map<String, dynamic> body) async {
-    final url = Uri.parse('https://places.googleapis.com/v1/$endpoint');
-    final headers = {
-      'Content-Type': 'application/json',
-      'X-Goog-Api-Key': _apiKey,
-      'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.location,places.id'
-    };
-
-    try {
-      final response =
-          await http.post(url, headers: headers, body: jsonEncode(body));
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      } else {
-        throw Exception('Failed with status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to send POST request: $e');
-    }
-  }
-
-  /// Google Places API に GET リクエストを送信
-  Future<Map<String, dynamic>> get(
-      String endpoint, Map<String, String> queryParams) async {
-    final query =
-        queryParams.entries.map((e) => '${e.key}=${e.value}').join('&');
-    final url = Uri.parse('https://places.googleapis.com/v1/$endpoint?$query');
-    final headers = {
-      'Content-Type': 'application/json',
-      'X-Goog-Api-Key': _apiKey,
-     // 'X-Goog-FieldMask': 'id,displayName,formattedAddress,currentOpeningHours,rating,userRatingCount,websiteUri,reviews,location'
-    };
-
-    try {
-      final response = await http.get(url, headers: headers);
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      } else {
-        throw Exception('Failed with status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to send GET request: $e');
-    }
-  }
+  @GET("places/{placeId}")
+  Future<GetPlaceDetailsResponse> getPlaceDetails(
+    @Path("placeId") String placeId,
+    @Queries() Map<String, dynamic> queryParams,
+    @Header("X-Goog-Api-Key") String apiKey,
+    @Header("Content-Type") String contentType);
 }
